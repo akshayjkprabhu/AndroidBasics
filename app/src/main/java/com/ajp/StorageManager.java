270672c3d67f2364d95b7431f61b697311b70d16
akshay <akshay.prabhu@robosoftin.com>
2019-03-12 15:39:42 +0530

    Safetynet

---
+++ app/src/main/java/com/ajp/StorageManager.java
@@ -0,0 +1,9 @@
+package com.ajp;
+
+/*
+ * Created by akshay on 8/3/19.
+ * Copyright © 2018, Huffy
+ * Written under contract by Robosoft Technologies Pvt. Ltd.
+ */
+public class StorageManager {
+}
